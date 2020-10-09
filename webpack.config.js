f8ab50ffda5231854063d01f212cf1d3493b10de
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-10-09 18:05:34 -0400

    webpack now creating manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
@@ -43,6 +45,21 @@ module.exports = {
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: 'static'
+    }),
+    new WebpackPwaManifest({
+      name: "Food Festival",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      start_url: "../",
+      fingerprints: false,
+      inject: false,
+      icons: [{
+        src: path.resolve("assets/img/icons/icon-512x512.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
     })
   ],
   mode: 'development'
